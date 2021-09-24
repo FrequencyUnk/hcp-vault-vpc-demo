c6b8800a54445fa604d23de41c33e6d00759f7c2
FrequencyUnk <77994041+FrequencyUnk@users.noreply.github.com>
2021-09-24 17:56:13 -0500

    Create transit-gateway.tf

---
+++ transit-gateway.tf
@@ -0,0 +1,41 @@
+resource "aws_vpc_peering_connection_accepter" "peer" {
+  vpc_peering_connection_id = hcp_aws_network_peering.peer.provider_peering_id
+  auto_accept               = true
+}
+
+resource "aws_ec2_transit_gateway" "example" {
+  tags = {
+    Name = "learn-hcp-tgw"
+  }
+}
+
+resource "aws_ram_resource_share" "example" {
+  name                      = "learn-hcp-resource-share"
+  allow_external_principals = true
+}
+
+resource "aws_ram_principal_association" "example" {
+  resource_share_arn = aws_ram_resource_share.example.arn
+  principal          = hcp_hvn.learn_hcp_vault_hvn.provider_account_id
+}
+
+resource "aws_ram_resource_association" "example" {
+  resource_share_arn = aws_ram_resource_share.example.arn
+  resource_arn       = aws_ec2_transit_gateway.example.arn
+}
+
+resource "hcp_aws_transit_gateway_attachment" "example" {
+  depends_on = [
+    aws_ram_principal_association.example,
+    aws_ram_resource_association.example,
+  ]
+
+  hvn_id                        = hcp_hvn.learn_hcp_vault_hvn.hvn_id
+  transit_gateway_attachment_id = "learn-hcp-tgw-attachment"
+  transit_gateway_id            = aws_ec2_transit_gateway.example.id
+  resource_share_arn            = aws_ram_resource_share.example.arn
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment_accepter" "example" {
+  transit_gateway_attachment_id = hcp_aws_transit_gateway_attachment.example.provider_transit_gateway_attachment_id
+}
